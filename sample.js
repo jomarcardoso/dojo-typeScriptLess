800e3295d587ff703e80e08165e8c3344dfb7ea7
jomarcardoso <antonionetocardoso@gmail.com>
2022-12-22 18:20:16 -0300

    sample.js

--- sample.js
+++ sample.js
@@ -1,25 +1,23 @@
-class Author {
-  constructor(name = '', bornDay) {
-    this.name = name;
-    this.bornDay = bornDay;
-  }
-}
-
 const POST_TYPE = {
   politics: 'politics',
   economy: 'economy',
-  violence: 'violence'
-}
+  violence: 'violence',
+};
 
 class Post {
   /**
-   * @param {Author} author 
-   * @param {keyof POST_TYPE} type 
+   * @param {Author} author
+   * @param {keyof POST_TYPE} type
    */
-  constructor(title = '', description = '', imageUrl = '', author, type, publicationDate = new Date()) {
+  constructor(
+    title = '',
+    description = '',
+    author,
+    type = POST_TYPE.economy,
+    publicationDate = new Date(),
+  ) {
     this.title = title;
     this.description = description;
-    this.imageUrl = imageUrl;
     this.author = author;
     this.type = type;
     this.publicationDate = publicationDate;
@@ -28,7 +26,7 @@ class Post {
 
 class Blog {
   /**
-   * @param {Post[]} posts 
+   * @param {Post[]} posts
    */
   constructor(posts = []) {
     this.posts = posts;
@@ -42,14 +40,50 @@ class Blog {
   }
 }
 
+/**
+ * @abstract
+ */
+class Admin {
+  constructor() {
+    if (this.constructor == Admin) {
+      throw new Error("Abstract classes can't be instantiated.");
+    }
+  }
+
+  /**
+   * @abstract
+   * @param {Blog} blog
+   * @param {keyof POST_TYPE} type
+   */
+  toPost(blog, title = '', description = '', type = POST_TYPE.economy) {
+    throw new Error('Method must be implemented.');
+  }
+}
+
+class Author extends Admin {
+  constructor(name = '', bornDay) {
+    this.name = name;
+    this.bornDay = bornDay;
+  }
+
+  /**
+   * @override
+   * @param {Blog} blog
+   * @param {keyof POST_TYPE} type
+   */
+  toPost(blog, title = '', description = '', type = POST_TYPE.economy) {
+    const post = new Post(title, description, this, type);
+
+    blog.addPost(post);
+  }
+}
+
 const jomarAuthor = new Author('Jomar', new Date('3/27/1991'));
-const ukrainPost = new Post(
-  'Atendo contra Ucrânia',
-  'russia lança bombas de fragmentação sobre a população ucraniana',
-  'image.png',
-  jomarAuthor,
-  'violence'
-);
 const blog = new Blog();
 
-blog.addPost(ukrainPost);
+jomarAuthor.toPost(
+  blog,
+  'Atentado contra Ucrânia',
+  'Russia lança bombas de fragmentação sobre a população ucraniana.',
+  'violence',
+);
